b32dc094a74e14c00c244a087dd561d01e6090b0
Amanda Ackerman <ackerm44@gmail.com>
2017-10-05 14:57:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init(e) {
-  const input = document.querySelector('input');
-
   document.addEventListener('keydown', function(e) {
     const key = parseInt(e.which || e.detail);
 
