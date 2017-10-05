90973f5313dfb3727995ba99cba49f35339024d2
Amanda Ackerman <ackerm44@gmail.com>
2017-10-05 14:20:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,19 +3,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init(e) {
-  const key = parseInt(e.detail || e.which);
+  document.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
 
-  if (key === code[index]) {
-    index++;
+    if (key === code[index]) {
+      index++;
 
-    if index === code.length) {
-      alert("Konami Code Found!");
+      if index === code.length) {
+        alert("Konami Code Found!");
 
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0; 
-  }
-
+  })
 
 }
